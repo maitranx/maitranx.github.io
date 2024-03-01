a5ec16041b78187576d7b44915e3f61b6d82b577
Son Nguyen <sonnguyentruong87@gmail.com>
2024-03-01 22:03:11 +0700

    replaced/added in more images/icons changed label/text fixed font size,color and layout

--- src/components/projects/ProjectSingle.vue
+++ src/components/projects/ProjectSingle.vue
@@ -16,14 +16,14 @@ export default {
                 <p class="font-general-semibold text-xl text-ternary-dark dark:text-ternary-light font-semibold mb-2">
                     {{ project.title }}
                 </p>
-                <span class="font-general-regular text-sm text-ternary-dark dark:text-ternary-light">{{ project.category }}</span>
+                <span class="font-general-regular text-sm text-ternary-dark dark:text-slate-300">{{ project.category }}</span>
             </div>
             <div class="px-4 py-6 border-t border-b border-neutral-700 justify-start items-center" 
                 v-if="project.isSkateHidden !== true">
                 <div class="flex justify-between items-center">
                     <div class="flex items-center">
                         <div class="text-sm text-ternary-dark dark:text-ternary-light">Stake</div>
-                        <img src="@/assets/icons/figma.svg" class="w-10 ml-4" alt="Light Logo" /> 
+                        <img src="@/assets/icons/Figma_logo.svg" class="w-10 ml-4" alt="Light Logo" /> 
                         <span class="text-neutral-200 text-xs font-semibold ml-2">Figma</span>
                         <img :src="project.skate" class="w-18 ml-4" alt="Light Logo" />
                         <span v-if="project.skate && project.skate.includes('bpi')" 

49c012cf3fa2708fb1ca46cdf68773c6d4a34bbb
Son Nguyen <sonnguyentruong87@gmail.com>
2024-03-03 19:22:06 +0700

    another fix for style (text color)

--- src/components/projects/Workshop.vue
+++ src/components/projects/Workshop.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="px-[24px] md:px-[120px] xl:px-[345px]">
         <div class="text-white flex flex-col items-center">
-            <div class="text-slate-300 text-base font-normal leading-snug max-w-[1104px]">
-                At FPT Design, team members are the most valuable asset. Through knowledge sharing and skill development initiatives, I aim to create an environment where everyone can thrive, grow, and contribute to our shared success.
+            <div class="text-slate-300 text-base font-normal leading-snug max-w-[1104px] text-slate-300">
+                At FPT Design, team members are the most valuable asset. 
+                Through knowledge sharing and skill development initiatives, 
+                I aim to create an environment where everyone can thrive, grow, and contribute to our shared success.
             </div>
-            <div class="grid grid-cols-3 xs:grid-cols-1 w-full max-w-[1104px] mt-5 gap-3">
+            <div class="grid grid-cols-3 w-full max-w-[1104px] mt-5 gap-3">
                 <div><img src="@/assets/images/projects/Internal_workshop_1.jpg" alt="Interal workshop" class="w-full" /></div>
                 <div><img src="@/assets/images/projects/Internal_workshop_2.jpg" alt="Interal workshop" class="w-full" /></div>
                 <div><img src="@/assets/images/projects/Internal_workshop_3.jpg" alt="Interal workshop" class="w-full" /></div>

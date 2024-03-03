046ef0ff09c6760e6a13ea01e9581f6123aaba51
Tien Nguyen <trongtien000m@gmail.com>
2024-03-03 10:30:43 +0700

    add luna static page

--- src/components/projects/Luna.vue
+++ src/components/projects/Luna.vue
@@ -1,5 +1,34 @@
 <template>
-    <div class="flex justify-center pb-10 text-lg text-white">
-        waiting for design
+    <div class="2xl:px-[240px]">
+        <img :src="imageSrc" />
     </div>
 </template>
+<script>
+export default {
+    data: () => ({
+        imageSrc: '',
+    }),
+    mounted() {
+        this.updateImageSource();
+        window.addEventListener('resize', this.updateImageSource);
+    },
+    beforeUnmount() {
+        window.removeEventListener('resize', this.updateImageSource);
+    },
+    methods: {
+        updateImageSource() {
+            const screenWidth = window.innerWidth;
+            // Define your breakpoints and image sources
+            if (screenWidth <= 375) {
+                this.imageSrc = require('@/assets/images/luna/Luna-mobile.png');
+            } else if (screenWidth <= 768) {
+                this.imageSrc = require('@/assets/images/luna/Luna-tablet.png');
+            } else if (screenWidth <= 1440) {
+                this.imageSrc = require('@/assets/images/luna/Luna-laptop.png');
+            } else {
+                this.imageSrc = require('@/assets/images/luna/Luna-desktop.png');
+            }
+        },
+    },
+};
+</script>

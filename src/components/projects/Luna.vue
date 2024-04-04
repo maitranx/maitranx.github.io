a9fd8b3c93e1252065a3da5821932f4ff26caca3
Sherry <vythao219@gmail.com>
2024-04-04 21:46:16 +0700

    change an image of luna

--- src/components/projects/Luna.vue
+++ src/components/projects/Luna.vue
@@ -4,12 +4,7 @@
         <div class="flex xl:flex-row flex-col bg-color-[#f5f5f5]">
             <img :src="imageEndSrc" class="w-full h-auto w-auto" v-if="imageEndSrc"/>
             <div class="lg:py-[80px] lg:px-[68px] md:py-[100px] md:px-[114px] p-[16px] bg-white justify-center items-center gap-2.5 inline-flex">
-                <div class="lg:py-[143px] lg:px-[60px] md:py-[122px] md:px-[48px] py-[58px] px-[32px] bg-zinc-100 justify-center items-center gap-2.5 flex">
-                    <div class="grow shrink basis-0 text-center text-black text-xl font-normal font-['Inter'] break-all">
-                       <a href="https://drive.google.com/file/d/1W4xIzSbJnwTh0G0PWj2SFUcDih8CGJ6F/view?usp=share_link" target="_blank"> https://drive.google.com/file/d/1W4xIzSbJnwTh0G0PWj2SFUcDih8CGJ6F/view?usp=share_link
-                    </a>
-                    </div>
-                </div>
+                <img src="@/assets/images/luna/Luna_mvp.gif" class="w-full h-auto w-auto"/>
             </div>
         </div>
     </div>
